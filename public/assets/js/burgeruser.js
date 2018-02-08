6c47ee92268210757de3584e68a84d1acf294971
Doreen Wiggins <Wiggy77@aol.com>
2018-02-07 22:21:41 -0500

    up and running

--- public/assets/js/burgeruser.js
+++ public/assets/js/burgeruser.js
@@ -1,46 +1,46 @@
 $(function () {
-            $(".create-form").on("submit", function (event) {
-                // Make sure to preventDefault on a submit event.
-                event.preventDefault();
-                console.log("it clicked");
-                var formatName = $("#yum").val().trim();
-                var name = formatName.toUpperCase();
-                var newBurger = {
-                    name: name
-                };
+    $(".create-form").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+        console.log("it clicked");
+        var formatName = $("#yum").val().trim();
+        var name = formatName.toUpperCase();
+        var newBurger = {
+            name: name
+        };
 
-                // Send the POST request.
-                $.ajax("/api/burgers", {
-                    type: "POST",
-                    data: newBurger
-                }).then(
-                    function () {
-                        console.log("created new burger");
-                        // Reload the page to get the updated list
-                        location.reload();
-                    }
-                );
-            });
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 
-                $(".eatBurger").on("click", function (event) {
-                    var id = $(this).data("id");
-                    var newMeal = $(this).data("newmeal");
+    $(".eatBurger").on("click", function (event) {
+        var id = $(this).data("id");
+        var newMeal = $(this).data("newmeal");
+        console.log("I'm in button click");
 
+        var newMealState = {
+            devoured: true
+        };
 
-                    var newMealState = {
-                        devoured: true
-                    };
-
-                    // Send the PUT request.
-                    $.ajax("/api/burgers/" + id, {
-                        type: "PUT",
-                        data: newMealState
-                    }).then(
-                        function () {
-                            console.log("You Ate It!");
-                            // Reload the page to get the updated list
-                            location.reload();
-                        }
-                    );
-                });
-            });
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newMealState
+        }).then(
+            function () {
+                console.log("You Ate It!");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
