83c0d9f88aafaa1c0c1d4ebd8771b116be0f9c55
Doreen Wiggins <Wiggy77@aol.com>
2018-03-15 23:54:03 -0400

    schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,3 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 -- creating our burgers table
-
-CREATE TABLE burgers (
-
-    id INT NOT NULL AUTO_INCREMENT,
-    burger_name varchar
-    (100) NULL,
-    devoured BOOLEAN DEFAULT FALSE,
-    date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    primary key
-    (id)
-);
